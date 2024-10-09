c873101db4ec874b5d82c0e81460ee7ccdbf0cce
MXuanZero <MXuan_0@163.com>
2024-10-09 16:54:36 +0800

    fix: 编译脚本错误

--- tools/cmake_build.bat
+++ tools/cmake_build.bat
@@ -2,7 +2,7 @@
 set PROJ_DIR=%~dp0..
 
 if not exist "%PROJ_DIR%\build" (
-    call .\cmake_config.bat
+    call %PROJ_DIR%\tools\cmake_config.bat
 )
 
 cmake --build %PROJ_DIR%\build --config Debug --target all -j 18 --
--- tools/cmake_rebuild.bat
+++ tools/cmake_rebuild.bat
@@ -5,11 +5,11 @@ set PROJ_NAME=lv_mgr
 set PROJ_DIR=%~dp0..
 
 if exist "%PROJ_DIR%\build" (
-	call ./clean.bat
+    call %PROJ_DIR%\tools\clean.bat
 )
 
-call ./cmake_config.bat
-call ./cmake_build.bat
+call %PROJ_DIR%\tools\cmake_config.bat
+call %PROJ_DIR%\tools\cmake_build.bat
 
 cd %PROJ_DIR%\bin\
 .\%PROJ_NAME%.exe
